62877a3685344cae422341f0eee4bec70dd33bb6
tsoa-iangotiana <152261956+tsoa-iangotiana@users.noreply.github.com>
2024-05-17 10:38:58 +0300

    Sprint1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # FrameWork
+
+-Veuillez référencer dans votre projet le fichier.jar
+-Veuillez mettre dans un même package les controlleurs , en les annotant (@Annotation("Controller"))
+Dans votre fichier de configuration :
+    -Veuillez déclarer votre Servlet 
+    -Mettre un parametre d'initialisation du nom de package 
+    -Mettre comme valeur le nom du package de votre controlleur  
--- script.bat
+++ script.bat
@@ -1,12 +1,18 @@
-
 mkdir "mybin"
 set mybin="D:\Fianarana\S4\WebDyn\My Frame\FrameWork\mybin"
 set ref="D:\Fianarana\S4\WebDyn\My Frame\FrameWork\lib\*"
 
 @REM Compilation des fichiers dans le répertoire src et ses sous-répertoires
 for /r ".\src" %%f in (*.java) do (
-    javac -cp %ref% -d %mybin% "%%f"
+   copy "%%f" "mybin\%%~nf.java"
 )
+cd mybin
+javac -cp %ref% -d "../mybin" *.java
+ for /r "." %%f in (*.java) do (
+    del "%%f" 
+ )
+ cd .. 
+
 @REM Définition des chemins
 set bin="D:\Fianarana\S4\WebDyn\My Frame\Framework\bin"
 set mylib="D:/Fianarana/S4/WebDyn/My Frame/Test/lib"
@@ -15,5 +21,5 @@ set jar=my-sprint-zero.jar
 @REM Création du fichier JAR
 jar -cvf %jar% -C %mybin% .
 
-echo D | xcopy /q/s/y %jar% %mylib%
+echo D | xcopy /q/y %jar% %mylib%
 @echo Done
